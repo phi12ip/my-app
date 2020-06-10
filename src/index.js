5a985c3a78c146eb3ab77d23308ef97e55c900d5
Phi12ip <coderphilip@gmail.com>
2020-06-10 05:23:14 -0400

    changed Square from class to func

--- src/index.js
+++ src/index.js
@@ -2,23 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
-    return (
-      <button 
-        className="square" 
-        onClick={ () => this.setState({value: 'X'}) }
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={ () => this.props.onClick() }
+    >
+      {this.props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -27,6 +19,12 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
     }
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
   }
   
   renderSquare(i) {
